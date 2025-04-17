5080dae96feaffd823cb53c90de6e2cde2b9811f
Aditya Joshi <adityajoshi304@gmail.com>
2025-04-17 16:02:58 +0530

    build init

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ mongoose.connect(process.env.MONGODB_URI)
 // API routes
 app.use('/api/destinations', destinationRoutes); 
 app.use('/api/users', userRoutes);
+app.get('/', (req, res) => {
+  res.send('Welcome to the Travel API!');
+}
+);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
